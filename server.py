c08cbb0fc490481881980c86cfb13c5aae5f21ca
Leslie <mslesliewu@gmail.com>
2017-09-19 19:15:50 -0700

    added salt and hash to passwords

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, redirect, flash, request, session
 import re
+import os, binascii
 import datetime
 from mysqlconnection import MySQLConnector
+import md5
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 app=Flask(__name__)
 app.secret_key='poquitoPOQUITO'
 mysql = MySQLConnector(app, 'login_registration')
+salt = binascii.b2a_hex(os.urandom(15))
 
 @app.route('/')
 def index():
@@ -33,6 +36,7 @@ def validate():
             flash("First name can only contain letters")
             return redirect('/')
         else:
+            first_name = request.form['first_name']
             session['first_valid'] = True
 
         #LAST NAME VALIDATION
@@ -43,6 +47,7 @@ def validate():
             flash("Last name can only contain letters")
             return redirect('/')
         else:
+            last_name = request.form['last_name']
             session['last_valid'] = True
 
         #EMAIL VALIDATION
@@ -53,9 +58,11 @@ def validate():
             flash("Email must be valid")
             return redirect('/')
         else:
+            email = request.form['email']
             session['email_valid'] = True
 
         #PASSWORD VALIDATION
+        
         if len(request.form['password']) < 1:
             flash("Password cannot be empty")
             return redirect('/')
@@ -63,6 +70,8 @@ def validate():
             flash("Password must be at least 8 characters long")
             return redirect('/')
         else:
+            password = request.form['password']
+            hash_pass = md5.new(password + salt).hexdigest()
             session['password_valid'] = True
 
         #CONFIRMATION VALIDATION
@@ -71,8 +80,9 @@ def validate():
             return redirect('/')
 
         if (session['first_valid'] and session['last_valid'] and session['email_valid'] and session['password_valid']):
-            query = "INSERT INTO users (first_name, last_name, email, password, created_at) VALUES ('" + request.form['first_name'] + "', '" + request.form['last_name'] + "', '" + request.form['email'] + "', '" + request.form['password'] + "', NOW());"
-            mysql.query_db(query)
+            insert_query = "INSERT INTO users (first_name, last_name, email, password, created_at) VALUES (:first_name, :last_name, :email, :hash_pass, NOW());"
+            query_data = {'first_name': first_name, 'last_name': last_name, 'email':email, 'hash_pass':hash_pass,}
+            mysql.query_db(insert_query, query_data)
             session['is_logged_in'] = True
 
     ##LOGIN VALIDATION
@@ -87,13 +97,8 @@ def validate():
         if (mysql.query_db(query)):
             session['email_valid'] = True
             query2 = "SELECT password FROM users WHERE email = '" + request.form['email'] + "';"
-            print query
-            print query2
-            print mysql.query_db(query)
-            print mysql.query_db(query2)
-            print request.form['email']
-            print request.form['password']
-            if (mysql.query_db(query2)[0]['password'] == request.form['password']): 
+            
+            if (mysql.query_db(query2)[0]['password'] == md5.new(request.form['password'] + salt).hexdigest()): 
                 session['password_valid'] = True
             else:
                 flash ("Password is incorrect")
@@ -108,5 +113,5 @@ def validate():
     ##CHECK TO SEE IF CAN BE LOGGED IN
     if (session['is_logged_in']):
         return redirect('/home')
-
+#FIX PASSWORD TO MATCH HASHED FOR LOGIN
 app.run(debug=True)
